47b3888a70e52fa2425cae496388d6ee1a74a617
Todd Eidson <todd@eidson.info>
2017-12-08 05:21:01 -0500

    Rename output file, minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename');
 
-gulp.task('styles', function() {
+gulp.task('sass', function() {
     gulp.src('./src/styles.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({format: 'beautify'}))
+        .pipe(rename('bootstrap-wireframe.css'))
         .pipe(gulp.dest('./dist/'));
 });
+
+gulp.task('minify-css', () => {
+    gulp.src('./dist/bootstrap-wireframe.css')
+      .pipe(cleanCSS())
+      .pipe(rename('bootstrap-wireframe.min.css'))
+      .pipe(gulp.dest('./dist/'));
+  });
+
+  gulp.task('build', ['sass', 'minify-css'])
