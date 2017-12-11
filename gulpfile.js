613e698850c436e1ca27d1cbcb61f039c7b9dfee
Todd Eidson <todd@eidson.info>
2017-12-11 06:08:09 -0500

    Renamed compilied files

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,14 @@ gulp.task('sass', function() {
     gulp.src('./src/styles.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(cleanCSS({format: 'beautify'}))
-        .pipe(rename('bootstrap-wireframe.css'))
+        .pipe(rename('bootstrap-wireframes.css'))
         .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('minify-css', () => {
-    gulp.src('./dist/bootstrap-wireframe.css')
+    gulp.src('./dist/bootstrap-wireframes.css')
       .pipe(cleanCSS())
-      .pipe(rename('bootstrap-wireframe.min.css'))
+      .pipe(rename('bootstrap-wireframes.min.css'))
       .pipe(gulp.dest('./dist/'));
   });
 
